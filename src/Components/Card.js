a03ab312b04df4959987dec3344ecbdf2890fad3
unknown <joakorbc@gmail.com>
2023-07-17 14:54:57 -0300

    working in card.js to do the gallery

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,17 +1,22 @@
-import "../App.css"
+import React from 'react';
+import "../App.css";
 
-const Card = () => { 
-    return(
-<div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title"></h5>
-    <p class="card-text"></p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-)
+const Card = ({ image, h5, txt, setSelectedMovie, movie }) => {
 
+  const handlePlayClick = () => {
+    setSelectedMovie(movie);
+  }
+
+  return (
+    <div className="card" style={{ width: "18rem" }}>
+      <img src={image} className="card-img-top" alt="IMG NOT FOUND" />
+      <div className="card-body">
+        <h5 className="card-title text-white">{h5}</h5>
+        <p className="card-text">{txt}</p>
+        <button href="#" className="btn btn-primary" onClick={handlePlayClick}>Play</button>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
