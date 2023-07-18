be64933aab83a4c0b595c5dd61ff4686cb805561
unknown <joakorbc@gmail.com>
2023-07-18 14:26:19 -0300

    adding link to card

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,19 +3,17 @@ import "../App.css";
 
 const Card = ({ image, h5, txt, setSelectedMovie, movie }) => {
 
+  const handlePlayClick = () => {
+    setSelectedMovie(movie);
+  }
+
   return (
     <div className="card" style={{ width: "18rem" }}>
       <img src={image} className="card-img-top" alt="IMG NOT FOUND" />
       <div className="card-body">
         <h5 className="card-title text-white">{h5}</h5>
         <p className="card-text">{txt}</p>
-        <Link
-          to={{
-            pathname: "/movie-details",
-            state: { movie }
-          }}
-          className="btn btn-primary"
-        ></Link>
+        <button href="#" className="btn btn-primary" onClick={handlePlayClick}>Play</button>
       </div>
     </div>
   );
