1a3d21f48c6dd5941ff2823623fb716928378add
unknown <joakorbc@gmail.com>
2023-07-18 12:22:31 -0300

    funtional detailsPage

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,18 +3,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';//packages
 
 import { initStorage } from '../Service/LocalStorageManager'; //functions
+import { downloadMovieList } from '../Service/TMDBManager';
 
 import RegisterPage from './RegisterPage'; //components
+import MovieDetailsPage from './MovieDetailsPage';
 
 
 function App() {
   initStorage()
-
+  
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<RegisterPage />} />
+          <Route path="/registerPage" element={<RegisterPage />} />
+          <Route path="/movies/:id" Component={MovieDetailsPage} />
         </Routes>
       </Router>
     </div>
--- src/Components/MovieDetailsPage.js
+++ src/Components/MovieDetailsPage.js
@@ -1,23 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { downloadMovie } from "../Service/TMDBManager";
 
-function MovieDetailsPage({movie}){
-    
-    const [movie, setMovie] = useState();
-    downloadMovie(setMovie, 278)
-        return (
-            <div className="movie-details">
-              <h1>{movie.title}</h1>
-              <img
-                src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-                alt={movie.title}
-              />
-              <p>{movie.overview}</p>
-              <p>Release Date: {movie.release_date}</p>
-              <p>Vote Average: {movie.vote_average}</p>
-              <p>Vote Count: {movie.vote_count}</p>
-            </div>
-          );
+function MovieDetailsPage() {
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    downloadMovie(setMovie, 278);
+  }, []);
+
+  console.log(movie);
+
+  return (
+    <div className="movie-details">
+      {movie ? (
+        <>
+          <h1>{movie.title}</h1>
+          <img
+            src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+            alt={movie.title}
+          />
+          <p>{movie.overview}</p>
+          <p>Release Date: {movie.release_date}</p>
+          <p>Vote Average: {movie.vote_average}</p>
+          <p>Vote Count: {movie.vote_count}</p>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 }
 
-export default MovieDetailsPage
+export default MovieDetailsPage;
